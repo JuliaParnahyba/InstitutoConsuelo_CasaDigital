252a99e3b1a8ae21b0ccd2c352ebaf33356a81df
JuliaParnahyba <prog.jump@gmail.com>
2025-12-06 17:11:59 -0300

    chore: organização de diretórios e documentos

---
+++ modulo02/Aula02_25.10.04/Assincrono/hard/hard.py
@@ -0,0 +1,14 @@
+"""
+Crie um sistema completo de gerenciamento de biblioteca que:
+
+- Tenha classes para `Livro`, `Usuario` e `Biblioteca`
+- Permita cadastrar livros e usuários
+- Implemente empréstimo e devolução
+- Salve todos os dados em arquivos JSON
+- Tenha funcionalidade para:
+  - Gerar relatório de livros mais emprestados (CSV)
+  - Buscar informações de livros em uma API pública (ex: Open Library)
+  - Importar lista de livros de um CSV
+- Use tratamento de exceções adequado
+- Implemente log de operações em arquivo de texto
+"""
